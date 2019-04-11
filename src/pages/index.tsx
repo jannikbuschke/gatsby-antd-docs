96ac8b0e1bb773247ca92bab213bbbe9c53524ae
Jannik Buschke <jannik.buschke@gmail.com>
2019-04-11 12:35:18 +0200

    fix layout on landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,46 @@
 import React from 'react'
-import { RootLayout as Layout } from '../Layout'
 import Button from 'antd/lib/button'
 import 'antd/lib/button/style/css'
 import { Link } from 'gatsby'
+import { Header } from '../Header'
 
 const IndexPage = () => {
   return (
-    <Layout>
-      <div>
-        <div align="center">
-          <p
-            style={{
-              color: 'cornflowerblue',
-              fontSize: 50,
-              fontWeight: 'bold',
-            }}
-          >
-            Gatsby Antd Markdown Documentation Boilerplate
-          </p>
-          <h2>
-            A gatsby starter to manage and render documentation documents
-            (jsx/markdown based)
-          </h2>
-          <p>
-            This is a fork of{' '}
-            <a href="https://github.com/cvluca/gatsby-starter-markdown" />
-          </p>
-          <br />
-          <Link to="/docs/get-started/introduction">
-            <Button type="primary" size="large" style={{ marginRight: 10 }}>
-              Get Started
-            </Button>
-          </Link>
-          <Button
-            type="primary"
-            size="large"
-            href="https://github.com/jannikbuschke/gatsby-antd-docs"
-            target="_blank"
-          >
-            Github
+    <div>
+      <Header siteTitle={'gatsby-antd-docs'} />
+      <div align="center" style={{ padding: 80 }}>
+        <p
+          style={{
+            color: 'cornflowerblue',
+            fontSize: 50,
+            fontWeight: 'bold',
+          }}
+        >
+          Gatsby Antd Markdown Documentation Boilerplate
+        </p>
+        <h2>A gatsby starter to create documentation websites</h2>
+        <p>
+          This is a fork of{' '}
+          <a href="https://github.com/cvluca/gatsby-starter-markdown">
+            https://github.com/cvluca/gatsby-starter-markdown
+          </a>
+        </p>
+        <br />
+        <Link to="/docs/get-started/introduction">
+          <Button type="primary" size="large" style={{ marginRight: 10 }}>
+            Get Started
           </Button>
-        </div>
+        </Link>
+        <Button
+          type="primary"
+          size="large"
+          href="https://github.com/jannikbuschke/gatsby-antd-docs"
+          target="_blank"
+        >
+          Github
+        </Button>
       </div>
-    </Layout>
+    </div>
   )
 }
 
