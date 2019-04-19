31081cb2b5dbf75ca0c70583cc0b09148ff9a828
Jannik Buschke <jannik.buschke@gmail.com>
2019-04-19 23:51:40 +0200

    improve landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,43 @@
 import React from 'react'
-import Button from 'antd/lib/button'
-import 'antd/lib/button/style/css'
+// import Button from 'antd/lib/button'
+// import 'antd/lib/button/style/css'
 import { Link } from 'gatsby'
 import { Header } from '../Header'
+import { Button, PageHeader, Icon } from 'antd'
 
 const IndexPage = () => {
   return (
-    <div>
-      <Header siteTitle={'gatsby-antd-docs'} />
-      <div align="center" style={{ padding: 80 }}>
-        <p
-          style={{
-            color: 'cornflowerblue',
-            fontSize: 50,
-            fontWeight: 'bold',
-          }}
-        >
-          Gatsby Antd Markdown Documentation Boilerplate
-        </p>
-        <h2>A gatsby starter to create documentation websites</h2>
-        <p>
-          This is a fork of{' '}
-          <a href="https://github.com/cvluca/gatsby-starter-markdown">
-            https://github.com/cvluca/gatsby-starter-markdown
-          </a>
-        </p>
-        <br />
-        <Link to="/docs/get-started/introduction">
-          <Button type="primary" size="large" style={{ marginRight: 10 }}>
-            Get Started
-          </Button>
-        </Link>
+    <div align="center" style={{ padding: 80 }}>
+      <p
+        style={{
+          color: 'cornflowerblue',
+          fontSize: 50,
+          fontWeight: 'bold',
+        }}
+      >
+        Gatsby Antd Markdown Documentation Boilerplate
+      </p>
+      <h2>A gatsby starter to create documentation websites</h2>
+      <p>
+        This is a fork of{' '}
+        <a href="https://github.com/cvluca/gatsby-starter-markdown">
+          https://github.com/cvluca/gatsby-starter-markdown
+        </a>
+      </p>
+      <br />
+      <Button.Group size="large">
         <Button
-          type="primary"
           size="large"
           href="https://github.com/jannikbuschke/gatsby-antd-docs"
           target="_blank"
         >
           Github
+          <Icon type="github" />
         </Button>
-      </div>
+        <Button type="primary" size="large" style={{ marginRight: 10 }}>
+          <Link to="/docs/get-started/introduction">Get Started</Link>
+        </Button>
+      </Button.Group>
     </div>
   )
 }
