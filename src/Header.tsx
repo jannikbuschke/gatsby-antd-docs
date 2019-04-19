b96515a0b1c60821444b726a82ddfac4b7df88f9
Jannik Buschke <jannik.buschke@gmail.com>
2019-04-19 23:51:19 +0200

    add github and twitter links to header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'gatsby'
-import { Layout, Menu } from 'antd'
-
-const { Header: AntdHeader } = Layout
+import { Layout, Menu, Row, Icon, Col } from 'antd'
 
 interface Props {
   siteTitle: string
@@ -12,15 +10,28 @@ export class Header extends Component<Props> {
   render() {
     const { siteTitle } = this.props
     return (
-      <AntdHeader>
-        <Menu theme="dark" mode="horizontal">
+      <Row>
+        <Menu mode="horizontal">
           <Menu.Item>
-            <Link to="/" style={{ textDecoration: 'none' }}>
-              {siteTitle}
-            </Link>
+            <Link to="/docs/get-started/introduction/">{siteTitle}</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <a
+              href="https://github.com/jannikbuschke/gatsby-antd-docs"
+              target="_blank"
+            >
+              <Icon type="github" />
+              GitHub
+            </a>
+          </Menu.Item>
+          <Menu.Item>
+            <a href="https://twitter.com/jannikbuschke" target="_blank">
+              <Icon type="twitter" />
+              Twitter
+            </a>
           </Menu.Item>
         </Menu>
-      </AntdHeader>
+      </Row>
     )
   }
 }
